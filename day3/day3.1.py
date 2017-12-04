db3ba7b39ea32e2b055e4e541712e3df50065921
Turysaz <turysaz@posteo.org>
2017-12-04 22:54:23 +0100

    Finish day 3

---
+++ day3/day3.1.py
@@ -0,0 +1,42 @@
+
+def neighbours(pos):
+    vert = [(-1, 1), (0, 1), (1, 1),
+            (-1, 0),         (1, 0),
+            (-1,-1), (0,-1), (1,-1)]
+
+    return [(i[0]+pos[0], i[1]+pos[1]) for i in vert]
+
+def get_next_field(x,y, level, count):
+    s = count / (level*2)
+    print(x,y,s,count,level)
+    if s == 0 or s == 4: return x,y+1
+    if s == 1: return x-1,y
+    if s == 2: return x, y-1
+    if s == 3: return x+1, y
+
+def get_tile_greater_than(query):
+    grid = {(0,0) : 1}
+    x,y = 1,0
+    level_count = 1
+    level = 1
+    while True:
+        next_sum = sum([grid[p] for p in neighbours((x,y)) if p in grid])
+        if (next_sum) > max_value:
+            print(next_sum)
+            return (x,y)
+
+        grid.update({(x,y) : next_sum})
+
+        if (x,y) == (level,-level):
+            level += 1
+            level_count = 1
+            x += 1
+            continue
+
+        x, y = get_next_field(x,y,level, level_count)
+        level_count += 1
+
+
+max_value = 368078
+#max_value = 800
+print(get_tile_greater_than(max_value))
