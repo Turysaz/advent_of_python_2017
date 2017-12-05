14895da16c511b4637b0386e5b74d10aa2b829c9
Turysaz <turysaz@posteo.org>
2017-12-05 10:53:02 +0100

    Solution day 4 part 1

---
+++ day04/day4.py
@@ -0,0 +1,13 @@
+
+f = open("input.txt")
+lines = f.readlines()
+f.close()
+
+def is_valid(line):
+    words = set()
+    for w in line.split():
+        if w in words: return False
+        else: words.add(w)
+    return True
+
+print(sum([is_valid(p) for p in lines]))
