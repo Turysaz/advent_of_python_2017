a9b375ceea5e2f90c0fe3a3103401e671549fd93
Turysaz <turysaz@posteo.org>
2017-12-05 11:20:26 +0100

    day 5 part 1

---
+++ day05/day5.py
@@ -0,0 +1,17 @@
+
+f = open("input.txt")
+instructions = [int(num[:-1]) for num in f.readlines()]
+f.close()
+
+#instructions = [0,3,0,1,-3]
+
+count = 0
+pc = 0
+
+while  pc >= 0 and pc < len(instructions):
+    count += 1
+    pc_old = pc
+    pc += instructions[pc]
+    instructions[pc_old] += 1
+
+print(count)
