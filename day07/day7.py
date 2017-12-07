26c511c5dec86920c1c790c77d22bd24e385b07d
Turysaz <turysaz@posteo.org>
2017-12-07 14:09:01 +0100

    Turn to def ()

--- day07/day7.py
+++ day07/day7.py
@@ -3,20 +3,21 @@ f = open("input.txt")
 lines = [l.strip() for l in f.readlines()]
 f.close()
 
-relations = {}
+def get_root(lines):
+    relations = {}
+    for line in lines:
+        father = line[:line.find('(')-1].strip()
+        if not father in relations: relations.update({father:None})
+        if "->" in line:
+            children = line[line.find("->")+3:].replace(",","").split()
+            for child in children:
+                if child not in relations:
+                    relations.update({child:father})
+                else:
+                    relations[child] = father
 
-for line in lines:
-    father = line[:line.find('(')-1].strip()
-    if not father in relations: relations.update({father:None})
-    if "->" in line:
-        children = line[line.find("->")+3:].replace(",","").split()
-        for child in children:
-            if child not in relations:
-                relations.update({child:father})
-            else:
-                relations[child] = father
+    for child in relations:
+        if relations[child] == None:
+            return child
 
-for child in relations:
-    if relations[child] == None:
-        print child
-        break
+print get_root(lines)
