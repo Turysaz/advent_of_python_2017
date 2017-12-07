cac49a46ebe325cbe3d6e34aaa3ab54bb7b9d7db
Turysaz <turysaz@posteo.org>
2017-12-07 14:05:59 +0100

    Solution day 7 part 1

---
+++ day07/day7.py
@@ -0,0 +1,22 @@
+f = open("input.txt")
+#f = open("test-input.txt")
+lines = [l.strip() for l in f.readlines()]
+f.close()
+
+relations = {}
+
+for line in lines:
+    father = line[:line.find('(')-1].strip()
+    if not father in relations: relations.update({father:None})
+    if "->" in line:
+        children = line[line.find("->")+3:].replace(",","").split()
+        for child in children:
+            if child not in relations:
+                relations.update({child:father})
+            else:
+                relations[child] = father
+
+for child in relations:
+    if relations[child] == None:
+        print child
+        break
