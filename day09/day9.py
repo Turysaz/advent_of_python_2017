e9e6d7d7a30b3c7e7ccf9c4e726faacb88c90524
Turysaz <turysaz@posteo.org>
2017-12-09 17:55:47 +0100

    Solution day 9 part 1

---
+++ day09/day9.py
@@ -0,0 +1,46 @@
+
+f = open("input.txt")
+stream = f.read()
+f.close()
+
+#stream = "{{<ab>},{<ab>},{<ab>},{<ab>}}" #9
+#stream = "{{<!!>},{<!!>},{<!!>},{<!!>}}" #9
+#stream = "{{<a!>},{<a!>},{<a!>},{<ab>}}" #3
+#stream = "{{{},{},{{}}}}" #16
+#stream = "{{},{}}" # 5
+#stream = "{{{}}}" # 6
+#stream = "{{<!!>},{<!>},{<!>},{<a>}}" #5
+
+index = 0
+depth = 1
+garbage = False
+score = 0
+
+while index < len(stream):
+    c = stream[index]
+    index += 1
+
+    if c == "!":
+        index += 1
+        continue
+
+    if c == ">":
+        garbage = False
+        continue
+
+    if garbage : continue
+
+    if c == "<":
+        garbage = True
+        continue
+
+    if c == "{":
+        score += depth
+        depth += 1
+        continue
+
+    if c == "}":
+        depth -= 1
+        continue
+
+print score
