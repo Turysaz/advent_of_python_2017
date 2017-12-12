7e19d5edf3ca8e0650e1b108d46ac27192db1726
Turysaz <turysaz@posteo.org>
2017-12-12 12:12:57 +0100

    Add solution for day 12 part 1

---
+++ day12/day12.py
@@ -0,0 +1,23 @@
+def get_connections(lines):
+    nums = [[int(x) for x in l.replace("<->", "").replace(",", "").split()] for l in lines]
+    con = [(l[0], l[1:]) for l in nums]
+    connections = {}
+    for c in con: connections.update({c[0]:c[1]})
+    return connections
+
+def get_group_for(x, connections):
+    group = {0}
+    l = 0
+    while l < len(group):
+        l = len(group)
+        group_new = set()
+        for node in group:
+            group_new.add(node)
+            for c in connections[node]:
+                group_new.add(c)
+        group = group_new
+    return group
+
+f = open("input.txt")
+connections = get_connections(f.readlines())
+f.close()
