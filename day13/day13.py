f45e1ff587b2937380db9c44ce8c563065bc391e
Turysaz <turysaz@posteo.org>
2017-12-13 13:39:42 +0100

    Make part 1 a little nicer

--- day13/day13.py
+++ day13/day13.py
@@ -1,40 +1,39 @@
+
 f = open("../input/day13.txt")
-layers = [[int(x[0]), int(x[1])] for x in [l.split(": ") for l in f.readlines()]]
+lines = [[int(x[0]), int(x[1])] for x in [l.split(": ") for l in f.readlines()]]
 f.close()
 
-#layers = [
-#    [0, 3],
-#    [1, 2],
-#    [4, 4],
-#    [6, 4]
-#]
+lines2 = [
+    [0, 3],
+    [1, 2],
+    [4, 4],
+    [6, 4]
+]
 
 # init firewall
 ranges = {}
-scanner_pos = []
-scanner_dir = []
-for x in layers: ranges.update({x[0]:x[1]})
-for x in range(max(ranges)+1):
-    if x not in ranges:
-        ranges.update({x:0})
-    scanner_pos.append(0)
-    scanner_dir.append(1)
+positions = {}
+directions = {}
+for layer in lines:
+    ranges.update({layer[0]:layer[1]})
+    positions.update({layer[0]:0})
+    directions.update({layer[0]:1})
 
 # init package
-package_pos = 0
+pack_pos = 0
 total_severity = 0
 
-while package_pos < len(ranges):
-    print(package_pos, scanner_pos)
-    if scanner_pos[package_pos] == 0:
-        print("MATCH")
-        total_severity += package_pos * ranges[package_pos]
+while pack_pos < max(ranges)+1:
+    if pack_pos in positions:
+        if positions[pack_pos] == 0:
+            total_severity += pack_pos * ranges[pack_pos]
 
-    for i in range(len(ranges)):
-        scanner_pos[i] += scanner_dir[i]
-        if scanner_pos[i] == 0 or scanner_pos[i] == ranges[i]-1:
-            scanner_dir[i] *= -1
+    for i in positions:
+        positions[i] += directions[i]
+        if positions[i] == 0 or positions[i] == ranges[i]-1:
+            directions[i] *= -1
 
-    package_pos += 1
+    pack_pos += 1
+
 
 print(total_severity)
